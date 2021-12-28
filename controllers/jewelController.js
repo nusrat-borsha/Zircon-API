09fdd72cf71237a005ea1716883a0a206720c4d6
nusrat-borsha <nuameeen@gmail.com>
2021-12-29 02:43:01 +0600

    create user model

--- controllers/jewelController.js
+++ controllers/jewelController.js
@@ -72,6 +72,7 @@ exports.deleteJewel = catchAsync(async (req, res, next) => {
     if(!jewel){
       return next(new AppError(`No Jewel found with that ID`, 404));
     };
+    
     res.status(204).json({
       status : success,
       data: null
