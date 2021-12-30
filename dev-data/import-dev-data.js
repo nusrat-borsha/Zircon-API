575dca6d4e129377d9f782f43908e813a743dd39
nusrat-borsha <nuameeen@gmail.com>
2021-12-31 04:40:37 +0600

    import user data to db

--- dev-data/import-dev-data.js
+++ dev-data/import-dev-data.js
@@ -2,17 +2,14 @@ const dotenv = require('dotenv').config({ path: './config.env' });
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Jewel = require('../models/jewelModel');
+const User = require('../models/userModel');
 
 
 const jewels = JSON.parse(fs.readFileSync(`${__dirname}/jewelries.json`,'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8'));
 
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
-mongoose.connect(DB,{
-  useNewUrlParser : true,
-  useCreateIndex : true,
-  useFindAndModify : false,
-  useUnifiedTopology: true,
-}).then(()=> {
+mongoose.connect(DB,{}).then(()=> {
   console.log("DB connection succesful") 
 });
 
@@ -25,6 +22,8 @@ const importData = async () => {
     }catch(err){
         console.log(err);
         }
+    
+    process.exit();
 };
 
 const deleteData = async () => {
@@ -36,16 +35,52 @@ const deleteData = async () => {
     }catch(err){
         console.log(err);
         }
+
+    process.exit();
+};
+
+const userImportData = async () => {
+    try{
+
+        await User.create(users);
+        console.log("User data successfully loaded");
+
+    }catch(err){
+        console.log(err);
+        }
+
+    process.exit();
+};
+
+const userDeleteData = async () => {
+    try{
+
+        await User.deleteMany();
+        console.log("User data successfully deleted");
+
+    }catch(err){
+        console.log(err);
+        }
+
+    process.exit();
 };
 
 console.log(process.argv);
 // input : node dev-data/import-dev-data.js  --import
 // output: 
 //     '--import' <-- so if we write --import or --delete then it will carry out that function
+//input : node dev-data/import-dev-data.js  --deleteUser
+//node dev-data/import-dev-data.js  --importUser
 
 if (process.argv[2] === '--import'){
     importData();
 }
-else{
+else if (process.argv[2] === '--delete'){
     deleteData();
+}
+else if (process.argv[2] === '--importUser'){
+    userImportData();
+}
+else if (process.argv[2] === '--deleteUser'){
+    userDeleteData();
 }
--- models/userModel.js
+++ models/userModel.js
@@ -13,6 +13,11 @@ const userSchema = new mongoose.Schema({
       lowercase: true,
       validate: [validator.isEmail, 'Please provide a valid email']
     },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user'
+    },
     password: {
       type: String,
       required: [true, 'Please provide a password'],
@@ -28,6 +33,11 @@ const userSchema = new mongoose.Schema({
         },
         message: 'Passwords are not the same!'
       }
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      select: false
     }
 
 });
